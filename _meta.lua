af9724c07f0ee420227edad24e582e1ab27321e6
MJCopper <matt@thecoppers.net>
2026-05-26 06:24:44 +1000

    ReadME.md bump version to 2.0.2

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     name        = "casualkochess",
     fullname    = _("Casual Chess"),
-    description = _([[Casual Chess & Board Games for KOReader. Author: MJCopper. Version: 2.0.1]]),
+    description = _([[Casual Chess & Board Games for KOReader. Author: MJCopper. Version: 2.0.2]]),
 }
